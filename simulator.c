6f5c3e222b504886c1b7cb2a04492636648797d0
Mathen Jose <mathen@hotmail.co.uk>
2019-11-18 16:05:09 +0100

    Code to read binary file

--- simulator.c
+++ simulator.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 // simulator for RISC-V 
 
 // take input from binary file
@@ -8,3 +9,40 @@
 // run each instruction
 
 // save the registers to binary file
+
+
+void main(){
+
+    unsigned int memory [1000];
+    unsigned int * memory_pointer = &memory[0]; // pointer to the memory array
+    for(int i = 0; i < 1000; i++){
+        memory[i] = 0;
+    }
+    unsigned int * memory_pointer = &memory[0]; // pointer to the memory array
+    
+    unsigned int registers [32]; 
+    for(int i = 0; i < 32; i++){
+        registers[i] = 0;
+    }
+
+    int counter;
+    FILE *ptr_myfile;
+
+    ptr_myfile=fopen("addpos.bin","rb");
+    if (!ptr_myfile){
+        printf("Unable to open file!");
+        return 1;
+    }
+
+    /* Seek to the beginning of the file */
+   fseek(ptr_myfile, 0, SEEK_SET);
+
+    // change memory pointer in the while loop
+    while(fread(memory_pointer, sizeof(int), 1, ptr_myfile)){
+        memory_pointer++; // go to next memory location
+        printf_s("%x \n",&memory_pointer);
+    }
+    
+    fclose(ptr_myfile);
+
+}
