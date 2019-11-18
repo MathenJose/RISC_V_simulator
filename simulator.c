e278423e53b838035208d486c8ce69f329b93921
Mathen Jose <mathen@hotmail.co.uk>
2019-11-18 15:21:11 +0100

    Commented the initial structure

---
+++ simulator.c
@@ -0,0 +1,10 @@
+// simulator for RISC-V 
+
+// take input from binary file
+// save it to memory
+
+// initialise the register + memory
+
+// run each instruction
+
+// save the registers to binary file
